56f87f8106e7eac1e732badaa6c298ddaed853e7
nealfennimore <hi@neal.codes>
2016-01-24 21:29:19 -0800

    Add cssnano and use sourcemaps for css

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,10 +5,13 @@ module.exports =  (gulp, $, paths, u) => {
     return () => {
         gulp.src([paths.styles.src + '**/*.scss'])
             .pipe( $.plumber(u.notifier.error()) )
+            .pipe( $.sourcemaps.init() )
             .pipe( $.sass() )
             .pipe(
                 $.postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ])
             )
+            .pipe( $.cssnano() )
+            .pipe( $.sourcemaps.write('.') )
             .pipe(gulp.dest(paths.styles.dist))
             .pipe(browserSync.reload({
                 stream: true
